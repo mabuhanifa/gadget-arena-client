4f04f4df707fd0e93cb871738bce1de49e4be7ff
shourovahmd <moh.abuhanifa@gmail.com>
2022-05-07 21:26:50 +0600

    comp-done

---
+++ src/Component/Blogs/Blogs.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div>
+      <div>
+        <h1>Difference between javascript and nodejs</h1>
+        <p></p>
+      </div>
+      <div>
+        <h1>Differences between sql and nosql databases.</h1>
+        <p></p>
+      </div>
+      <div>
+        <h1>What is the purpose of jwt and how does it work</h1>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ src/Component/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Login;
